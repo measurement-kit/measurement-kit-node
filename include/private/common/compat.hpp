301996b548debae03c31970fafa638868b6dee1a
Simone Basso <bassosimone@gmail.com>
2017-10-31 12:24:25 +0100

    Require MK >= 0.7.11

--- include/private/common/compat.hpp
+++ include/private/common/compat.hpp
@@ -6,18 +6,23 @@
 
 #include <measurement_kit/common.hpp>
 
+// MK_VERSION_MAJOR was added in MK v0.7.10, will be part of v0.8.0-beta,
+// and was committed during the development of v0.9.0-dev. Releases before
+// the ones indicated above will not work with these bindings.
 #ifndef MK_VERSION_MAJOR
-#error "Too old version of measurement-kit"
+#error "Unsupported version of measurement-kit: MK_VERSION_MAJOR not defined"
 #endif
 
-// We have seen that with MK v0.7.10 most tests do not break out of libuv
-// loop, most likely because of reference loops. The only test that currently
-// works correctly is web connectivity. I wonder if it's because of the test
-// or because of the fact it's the only test with multiple input. I've also
-// seen that MK v0.9.0-dev has much less problems instead.
-#if MK_VERSION_MAJOR < 1 && MK_VERSION_MINOR < 9
-#warning "This addon will not work reliably with measurement-kit < v0.9.0"
+// Before MK v0.7.11, nodejs tests did not terminate because there was a self
+// reference that has been fixed in measurement-kit/measurement-kit#1453. It
+// is interesting to note that 1453 is also the year when the one hundred years
+// war actually finished (but I don't think that alone has fixed the issue).
+#if MK_VERSION_MAJOR < 1 && MK_VERSION_MINOR < 7 && MK_VERSION_PATCH < 11
+#error "MK >= 0.7.11 is required."
+#endif
 
+// Before MK v0.8.0-dev, SharedPointer was actually named Var.
+#if MK_VERSION_MAJOR < 1 && MK_VERSION_MINOR < 8
 namespace mk {
 template <typename T> using SharedPtr = Var<T>;
 } // namespace mk
