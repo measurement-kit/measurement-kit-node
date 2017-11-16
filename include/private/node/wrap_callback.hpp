6cc015892e1ad4985ce6d638323ac2ae07133207
Simone Basso <bassosimone@gmail.com>
2017-11-16 12:41:47 +0100

    bindings: refactor comments

--- include/private/node/wrap_callback.hpp
+++ include/private/node/wrap_callback.hpp
@@ -10,10 +10,10 @@
 namespace mk {
 namespace node {
 
-// The wrap_callback() free function is a syntactic sugar for converting
-// a v8::Value into a Nan::Callback. The latter is a persistent type suitable
-// for wrapping a function to be called at a later time. It turns out that
-// such callback must be called from Node's main loop (i.e. uv_run()).
+/// The wrap_callback() free function is a syntactic sugar for converting
+/// a v8::Value into a Nan::Callback. The latter is a persistent type suitable
+/// for wrapping a function to be called at a later time. It turns out that
+/// such callback must be called from Node's main loop (i.e. uv_run()).
 SharedPtr<Nan::Callback> wrap_callback(v8::Local<v8::Value> value) {
     return SharedPtr<Nan::Callback>{
             new Nan::Callback{value.As<v8::Function>()}};
